5070fbdaf0d86aa0b6e21068e419e5fe422f065e
tkdlrs <tkd.lsanchez@gmail.com>
2026-01-02 19:07:59 -0700

    Completes 'Ch 1: Config | L3: Config' of the 'Build a Blog Aggregator in TypeScript' guided project on Boot.dev.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
+import { setUser } from "./config.js";
+import { readConfig } from "./config.js";
+
 function main() {
-    console.log("Hello, world!");
-}
+    // Set current user and update config file on disk.
+    setUser("Levi");
+    // Read config again, parse it into object and print to terminal
+    const cfg = readConfig();
+    console.log(cfg);
+
+};
 
 main();
 
